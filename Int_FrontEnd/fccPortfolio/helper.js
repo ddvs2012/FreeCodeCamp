7772aa3a7be9ceda23008304f317e13b47a50c9a
Daniel Alexander <davisd49@msu.edu>
2017-02-02 21:45:44 -0500

    first commit

---
+++ Int_FrontEnd/fccPortfolio/helper.js
@@ -0,0 +1,15 @@
+document.getElementById("homeButton").onclick=function(){
+	document.getElementById("banner").scrollIntoView()
+};
+
+document.getElementById("aboutButton").onclick=function(){
+	document.getElementById("about").scrollIntoView()
+};
+
+document.getElementById("portfolioButton").onclick=function(){
+	document.getElementById("portfolio").scrollIntoView()
+};
+
+document.getElementById("contactButton").onclick=function(){
+	document.getElementById("contact").scrollIntoView()
+};
---
+++ Int_FrontEnd/randomQuoteGenerator/script.js
@@ -0,0 +1,86 @@
+ /*$(document).ready(function() {
+    // Only change code below this line.
+    $("#quoteBoxTwo").on("click", function(){
+		newQuote = Math.floor(Math.random() * quotes.length);
+		$(".message").html(quotes[newQuote]);
+    });
+    // Only change code above this line.
+  });
+  
+$(document).ready(function() {
+	 $("#clickableAwesomeFont").on("click", function(){
+		 location.href = "https://twitter.com/intent/tweet"
+	 });
+ });
+ 
+
+  quotes = [
+	"Wine is constant proof that God loves us and loves to see us happy. ",
+  
+	"By failing to prepare, you are preparing to fail. ",
+	
+	"Without continual growth and progress, such words as improvement, achievement, and success have no meaning. ",
+	"Early to bed and early to rise makes a man healthy, wealthy and wise.",
+	"Money has never made man happy, nor will it, there is nothing in its nature to produce happiness. The more of it one has the more one wants. ",
+	"Whatever is begun in anger ends in shame."
+	
+  ]
+  */
+
+  function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
+
+
+var currentQuote = '', currentAuthor = '';
+function openURL(url){
+  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+}
+function getQuote() {
+  $.ajax({
+    headers: {
+      "X-Mashape-Key": "lmUWR3jexGmshe1qbkeYt2hrahtOp1QCTIsjsnKvLnbpDB6ZcB",
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=movies',
+    success: function(response) {
+      var r = JSON.parse(response);
+      currentQuote = r.quote;
+      currentAuthor = r.author;
+      if(inIframe())
+      {
+        $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + authors[newAuthor]));
+      }
+      $("#quoteText").animate({
+          opacity: 100
+        }, 500,
+        function() {
+          $('#quoteText').html(r.quote);
+        });
+
+      $("#author").animate({
+          opacity: 100
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $('#author').text(r.author);
+        });
+
+     
+    }
+  });
+}
+$(document).ready(function() {
+  getQuote();
+  $('#getMessage').on('click', getQuote);
+  $('#tweet').on('click', function() {
+    if(!inIframe()) {
+      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+    }
+  });
+  
+  
+ 
+  
+});
